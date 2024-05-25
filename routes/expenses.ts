77c6a4d805591aad102f27be40c97bcd74002489
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2024-05-25 03:29:25 -0400

    Added Zod for Schema validations, to make sure that the input data matches the required typeSchema, even at the path level

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,24 +1,35 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { z } from "zod";
 
 type Expense = {
-    id: string;
+    id: number;
     title: string;
     amount: number;
 };
 
-const expenses: Expense[] = [
-    {id: '1', title: 'Groceries', amount: 100},
-    {id: '2', title: 'Rent', amount: 1000},
-    {id: '3', title: 'Car', amount: 5000},
+const fakeExpenses: Expense[] = [
+    {id: 1, title: 'Groceries', amount: 100},
+    {id: 2, title: 'Rent', amount: 1000},
+    {id: 3, title: 'Car', amount: 5000},
 ];
+
+const createExpenseSchema = z.object({
+    title: z.string().min(3).max(50),
+    amount: z.number().int().positive(),
+});
 
 // The endpoints can be chained togetheror created separately as done in an Node/Express app
 export const ExpensesRoutes = new Hono()
     .get('/', async c => {
-        return c.json(expenses)
+        return c.json(fakeExpenses)
     })
-    .post('/', async c => {
-        const expense:Expense = await c.req.json();
+    // zValidator() happens before the route is reached and made sure the data passed in is valid
+    .post('/', zValidator("json", createExpenseSchema), async c => {
+        const data = await c.req.valid("json"); // .json() is replaced with valid("json"), which contain anything that was validated before the route
+        // An Expense object is created from the data received from the request only if it matches the TS Object that it is expected to be, else it will throw an error
+        const expense = createExpenseSchema.parse(data);
+        fakeExpenses.push({ ...expense, id: fakeExpenses.length+1});
         console.log(expense);
         
         return c.json(expense);
