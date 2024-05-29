19ba79cd4f58586060e873908d436958e5e2293a
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2024-05-29 14:11:15 -0400

    Isolated the auth check from a single page and cached the user data, until manual invalidation, to prevent making requests to fetch user data, while visitn any protected route

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,26 @@
 
 import { hc } from 'hono/client'
 import { type ApiRoutes } from '@server/app'
+import { queryOptions } from '@tanstack/react-query';
 
 const client = hc<ApiRoutes>('/');
 
 export const { api } = client;
+
+async function getCurrentUser() {
+    const res = await api.me.$get();
+
+    if (!res.ok) { 
+        throw new Error('Server Error!');
+    }
+
+    const data = await res.json();
+    return data;
+}
+
+export const userQueryOptions = queryOptions({ 
+    queryKey: ['get-current-user'], 
+    queryFn: getCurrentUser,
+    // When one page uses this query, the data will be cached, to prevent and anyother pages to make the request. The data will be available until it is invalidated by the user by a page refresh or by logging out and logging in
+    staleTime: Infinity,
+});
--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -1,28 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useQuery } from '@tanstack/react-query';
-import { api } from '../lib/api';
+import { userQueryOptions } from '../lib/api';
 
 export const Route = createFileRoute('/profile')({
     component: Profile,
 })
 
-async function getCurrentUser() {
-    const res = await api.me.$get();
-
-    if (!res.ok) {
-        throw new Error('Server Error!');
-    }
-
-    const data = await res.json();
-    return data;
-}
-
 function Profile() {
 
-    const { isPending, data, error } = useQuery({ 
-        queryKey: ['get-current-user'], 
-        queryFn: getCurrentUser 
-    });
+    const { isPending, data, error } = useQuery(userQueryOptions);
 
     if(isPending){
         return 'Loading...'
